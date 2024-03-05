d697cf72e47f2a6cb3aab615b257e13099f8e6b9
Jhf3 <156826060+Jhf3@users.noreply.github.com>
2024-03-05 15:36:39 -0500

    Add files via upload

--- Update_html_File.py
+++ Update_html_File.py
@@ -4,23 +4,26 @@ import subprocess
 
 # use subprocess to run PHP file with directory as input
 # PHP file updates json file with new mp3 data
-subprocess.run(["mp3.php"], shell=True)
 
-json_file = open("mp3_obj.json", 'r') # open json file
-html_file = open("writeTo.html", 'w') # open html file
+# json_file = open("mp3_obj.json", 'r') # open json file
+html_file = open("html_output.html", 'w') # open html file
 output = ''
 
-json_data = json.loads(json_file.read()) # json_data is json object
+# get the json object from the php script by running the php script using subprocess and capturing the output as json_obj
+json_obj = subprocess.run(["mp3.php"], shell=True, capture_output = True, text = True)
+json_data = json.loads(json_obj.stdout) # json_data is json object
+
 # iterate through the json object
-for i in json_data['mp3_data']:
+for i in json_data['results']:
     # 'i' is a python dictionary containing "mp3_link" and "mp3_title" as keys,
     # both containing strings for the mp3 link and title respectively
     # use i to get the strings to create the html output
-    output += '<a href="' + i["mp3_link"] + '">' + i["mp3_title"] + "</a></p>\n"
+    output += '<a href="' + i["link"] + '">' + i["title"] + "</a></p>\n"
 
 # Write to the HTML file
 html_file.write(output)
 
 # Saving the data into the HTML file
-json_file.close()
+# json_file.close()
 html_file.close()
+
